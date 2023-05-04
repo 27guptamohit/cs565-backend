aeacb95ee7bc27cfe6daeb688f315ac6e341778d
Christopher Kull <chbkull@gmail.com>
2023-05-04 11:32:45 -0500

    User deletion script

--- scripts/full_reset.sh
+++ scripts/full_reset.sh
@@ -2,6 +2,7 @@
 
 python scripts/delete_sheets.py
 python scripts/delete_measures.py
+python scripts/delete_users.py
 python scripts/upload_sheet.py test_data/sheet1 sheet1
 python scripts/upload_sheet.py test_data/sheet2 sheet2
 python scripts/upload_sheet.py test_data/sheet3 sheet3
